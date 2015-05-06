866bfa551de8921775d9613ada5cbc26dd87c175
Craig Olson <craigolson@dhcp-10-101-129-203.wireless.northwestern.private>
2015-05-06 18:36:15 -0500

    demo ready

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,11 +5,16 @@ angular.module('travelbotApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
   
   $scope.places = [];
+  $scope.liked = [];
+  var index = 0;
 
   $http.get('/api/places').success(function(places) {
     $scope.places = places;
     socket.syncUpdates('place', $scope.places);
+    // Keep track of current place and only display that
+    $scope.place = $scope.places[0];
   });
+
 
   var cities = [{
     name: 'Chicago',
@@ -25,4 +30,39 @@ angular.module('travelbotApp')
   };
 
   $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  // User has liked the place
+  //  Added to $scope.liked array add shift to next place
+  //  At end, create itinerary
+  $scope.toggleLike = function() {
+    $scope.liked.push($scope.place[index]);
+    index++;
+    if (index >= $scope.places.length) {
+      $scope.place = {
+        name: 'Done!',
+        img: null,
+        description: ''
+      };
+
+    } else {
+      $scope.place = $scope.places[index];
+    }
+  };
+
+  // User has not liked the place
+  // Discard
+  $scope.toggleDislike = function() {
+    index++;
+    if (index >= $scope.places.length) {
+      $scope.place = {
+        name: 'Done!',
+        img: null,
+        description: ''
+      };
+
+    } else {
+      $scope.place = $scope.places[index];
+    }
+  };
+
 });
--- server/config/seed.js
+++ server/config/seed.js
@@ -20,5 +20,33 @@ Place.find({}).remove(function() {
       'It measures 33 by 66 by 42 feet (10 by 20 by 13 m), and weighs 110 short tons (100 t; 98 long tons).',
     lat: 41.8827,
     long: 87.6233
+  }, {
+    name: 'Weiner\s Circle',
+    img: 'assets/images/weinercircle.jpg',
+    description: 'The Wieners Circle is a hot dog stand in the Lincoln Park neighborhood of Chicago, Illinois, ' +
+      'United States.[1] It is famous for four things: its signature Maxwell Street Char-dogs, hamburgers, ' +
+      'cheese fries, and the mutual verbal abuse between the employees and the customers during the late-weekend hours.'
+  }, {
+    name: 'Green Mill',
+    img: 'assets/images/greenmill.jpeg',
+    description: 'The Green Mill Cocktail Lounge (or Green Mill Jazz Club) is an entertainment venue on Broadway in ' +
+      'Uptown, Chicago. It is known for its jazz and poetry performances, along with its connections to Chicago mob history.'
+  }, {
+    name: 'Willis Tower',
+    img: 'assets/images/willis_tower.jpg',
+    description: 'The Willis Tower, built as and still commonly referred to as Sears Tower, is a 108-story, 1,451-foot ' +
+      '(442 m) skyscraper in Chicago, Illinois, United States.[2] At completion in 1973, it surpassed the World Trade Center ' +
+      'towers in New York to become the tallest building in the world, a title it held for nearly 25 years. ' +
+      'The Willis Tower is the second-tallest building in the United States and the 12th-tallest in the world. ' +
+      'More than one million people visit its observation deck each year, making it one of Chicago\'s most popular tourist ' +
+      'destinations. The structure was renamed in 2009 by the Willis Group as part of its lease on a portion of the ' +
+      'tower\'s space.'
+  }, {
+    name: 'Art Institute of Chicago',
+    img: 'assets/images/art_institute.jpg',
+    description: 'The Art Institute of Chicago (AIC) is an encyclopedic art museum located in Chicago\'s Grant Park. ' +
+      'It features a collection of Impressionist and Post-Impressionist art in its permanent collection. Its holdings ' +
+      'also include American art, Old Masters, European and American decorative arts, Asian art, modern and contemporary ' +
+      'art, and architecture and industrial and graphic design. In addition, it houses the Ryerson & Burnham Libraries.'
   });
 });
