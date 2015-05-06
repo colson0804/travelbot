2682c80ec6d078fa6d690f1140c4a4b8dd0e1607
Craig Olson <craigolson@Craigs-MacBook-Pro.local>
2015-05-06 08:13:35 -0500

    removed things endpoint and moved map controller into main

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,28 @@
 'use strict';
+/*global google*/
 
 angular.module('travelbotApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+  
+  $scope.places = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+  $http.get('/api/places').success(function(places) {
+    $scope.places = places;
+    socket.syncUpdates('place', $scope.places);
   });
+
+  var cities = [{
+    name: 'Chicago',
+    latitude: 41.8731862,
+    longitude: -87.6253513,
+    zoom: 13
+  }];
+
+  var mapOptions = {
+      zoom: cities[0].zoom,
+      center: new google.maps.LatLng(cities[0].latitude, cities[0].longitude),
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+
+  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+});
