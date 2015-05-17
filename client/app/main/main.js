793a704e95d1721eaa6af11e8402221f0963907d
jeanettepranin <jeanettepranin@gmail.com>
2015-05-17 18:38:48 -0500

    Added landing page

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -4,7 +4,12 @@ angular.module('travelbotApp')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'app/main/main.html',
+        templateUrl: 'app/main/index.html',
         controller: 'MainCtrl'
-      });
+      })
+      .when('/makeyouritinerary', {
+      	templateUrl: 'app/main/main.html',
+      	controller: 'MainCtrl'
+      })
+      .otherwise({ redirectTo: '/' });
   });
