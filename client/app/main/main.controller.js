38d09a3d5918053afa743ccb519029ad6d33e348
Craig Olson <craigolson@dhcp-10-105-134-220.wireless.northwestern.private>
2015-06-03 15:55:49 -0500

    refactoring

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -49,16 +49,13 @@ angular.module('travelbotApp')
 function codeAddress() {
     //geocoder = new google.maps.Geocoder();
     var address = $scope.place.name + ' in Chicago';
-    console.log(address)
     $scope.geocoder.geocode( { 'address': address}, function(results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
-        console.log(results[0].geometry.location)
         $scope.map.panTo(results[0].geometry.location);
         addMarker(results[0].geometry.location);
       } 
 
       else {
-        console.log('did not find coordinates')
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
@@ -66,37 +63,32 @@ function codeAddress() {
 
   // Add a marker to the map and push to the array.
 function addMarker(location) {
-  console.log('adding marker to list')
-  //deleteMarkers();
   var marker = new google.maps.Marker({
     position: location,
     map: $scope.map
   });
   markers.push(marker);
-  console.log(markers)
   setAllMap($scope.map);
 }
 
 // Sets the map on all markers in the array.
 function setAllMap(map) {
   for (var i = 0; i < markers.length; i++) {
-    console.log(i)
     markers[i].setMap(map);
-    console.log(markers[i].setMap(map))
   }
 }
 
 
 // Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setAllMap(null);
-}
+// function clearMarkers() {
+//   setAllMap(null);
+// }
 
 // Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
-}
+// function deleteMarkers() {
+//   clearMarkers();
+//   markers = [];
+// }
 
  /*******************************************************/
  /* ITINERARY FUNCTIONS                                 */
@@ -108,18 +100,19 @@ function deleteMarkers() {
   $scope.toggleLike = function() {  
     updateTagProb();
     updateItinerary();
+    var counterString;
     if (itinIndex > maxSurvey) {
-      var counter_string = "pic" + counter.toString();
-      unfade(document.getElementById(counter_string));
+      counterString = 'pic' + counter.toString();
+      unfade(document.getElementById(counterString));
       $scope.moduleState = 'itinerary';
       //$scope.createItinerary();
     } else {
       codeAddress();
-      unfade(document.getElementById("place-name"));
-      unfade(document.getElementById("place-img"));
-      unfade(document.getElementById("description"));
-      var counter_string = "pic" + counter.toString();
-      unfade(document.getElementById(counter_string));
+      unfade(document.getElementById('place-name'));
+      unfade(document.getElementById('place-img'));
+      unfade(document.getElementById('description'));
+      counterString = 'pic' + counter.toString();
+      unfade(document.getElementById(counterString));
       counter++;
     }
   };
@@ -148,10 +141,10 @@ function deleteMarkers() {
             clearInterval(timer);
         }
         element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        element.style.filter = 'alpha(opacity=' + op * 100 + ')';
         op += op * 0.1;
     }, 10);
-  };
+  }
 
   // Increments the tag count for the 'liked' event
   function updateTagProb() {
@@ -162,7 +155,7 @@ function deleteMarkers() {
         $scope.tagProb[$scope.place.tags[i]] = 1;
       }
     }
-  };
+  }
 
   function scoring(arr) {
     // Determine a score for each place
@@ -187,7 +180,7 @@ function deleteMarkers() {
       return b.iconic - a.iconic;
     });
     return arr;
-  };
+  }
 
   function updateItinerary() {
       $scope.itinerary[itinIndex] = $scope.place;
@@ -204,7 +197,7 @@ function deleteMarkers() {
         //newLocation();
       }
       newLocation();
-  };
+  }
 
 
 
@@ -220,7 +213,7 @@ function deleteMarkers() {
       return !contains(el.tags, tag);
     });
     return ret;
-  };
+  }
 
   function contains(a, str) {
     for (var i = 0; i < a.length; i++) {
@@ -229,17 +222,17 @@ function deleteMarkers() {
         }
     }
     return false;
-  };
+  }
 
   function selectPlaceByTime(time) {
-    var arr = $scope.places
+    var arr = $scope.places;
     for (var i=0; i < arr.length; i++) {
       if (contains(arr[i].TimeTags, time)) {
         // Select first element matching this time tag
         return arr.splice(i, 1)[0];
       }
     }
-  };
+  }
 
   function selectFoodByTime(time) {
     var arr = $scope.food;
@@ -272,52 +265,44 @@ function deleteMarkers() {
   }
 
   function isMeal(index) {
-    if (index == 1 || index == 4 || index == 7 || index == 10) {
+    if (index === 1 || index === 4 || index === 7 || index === 10) {
       return true;
     } else {
       return false;
     }
-  };
+  }
 
   function isLunch(index) {
-    if (index == 1 || index == 7) {
+    if (index === 1 || index === 7) {
       return true;
     } else {
       return false;
     }
-  };
-
-  function isDinner(index) {
-    if (index == 4 || index == 10) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  }
 
   function isMorning(index) {
-    if (index == 0 || index == 6) {
+    if (index === 0 || index === 6) {
       return true;
     } else {
       return false;
     }
-  };
+  }
 
   function isAfternoon(index) {   // Afternoon or Evening
-    if (index == 2 || index == 3 || index == 8 || index == 9) {
+    if (index === 2 || index === 3 || index === 8 || index === 9) {
       return true;
     } else {
       return false;
     }
-  };
+  }
 
   function isNight(index) {
-    if (index == 5 || index == 11) {
+    if (index === 5 || index === 11) {
       return true;
     } else {
       return false;
     }
-  };
+  }
 
 
 
